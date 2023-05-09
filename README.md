197e88b656fcaf98152bca09bb2416c2c04c7286
Christian Eckardt <eckardt.christian@web.de>
2023-05-09 19:24:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Running Mario
+
+### About
+
+Running Mario adds an animated Mario sprite to your website. Click it to see what happens.
+
+### How to use
+
+Import **runMario** from the package and call the function. That's it.
---
+++ index.js
@@ -0,0 +1,91 @@
+function runMario() {
+    const marioContainer = document.createElement("div")
+
+    const mario = `
+        <div id="running-mario">
+            <div class="position-relative">
+            <img id="mario" src="https://eckeecke.github.io/images/mariosprites-walking.png">
+            </div>
+        </div>
+    `
+    
+    marioContainer.innerHTML = mario
+    
+    document.body.appendChild(marioContainer)
+    
+    const marioSound = new Audio("https://eckeecke.github.io/sounds/mario_its_me.wav")
+    
+    const marioBox = document.getElementById("running-mario");
+    
+    let marioLeft = -60;
+    
+    function movingBox(){
+      marioBox.style.transform = `translateX(${marioLeft}px)`;
+      if(marioLeft<window.innerWidth){
+        marioLeft += 2;
+      }
+    
+      if(marioLeft>=window.innerWidth){
+        marioLeft = -500;
+      }
+    }
+    
+    setInterval(movingBox, 1000/60);
+    
+    const marioSprite = document.getElementById("mario");
+    
+    let spritesheetPosition = 0;
+    let sheetMovement = 50;
+    
+    function animateMario(){
+      spritesheetPosition += sheetMovement;
+      marioSprite.style.transform = `translateX(${-spritesheetPosition}px)`;
+    
+      if(spritesheetPosition == 100){
+        sheetMovement = -50;
+      }
+    
+      if(spritesheetPosition == 0){
+        sheetMovement = 50;
+      }
+    }
+    
+    marioSprite.addEventListener("click", itsMeMario)
+
+    setInterval(animateMario, 102);
+
+    const head  = document.getElementsByTagName('head')[0];
+    const stylesheet  = document.createElement('style');
+    stylesheet.innerHTML = `
+      #running-mario {
+        position: fixed;
+        height: 60px;
+        width: 50px;
+        transform: translateX(-60px);
+        bottom: -5px;
+        overflow: hidden;
+        z-index: 5;
+      }
+      
+      #mario {
+        position: absolute;
+        right: -100px;
+        height: 60px;
+        width: 150px;
+        background-color: none;
+        cursor: pointer;
+      }
+    `
+    head.appendChild(stylesheet);
+    
+    
+    function itsMeMario(){
+      marioSound.volume = 0.2;
+      marioSound.play();
+    }
+
+}
+
+module.exports = runMario
+
+
